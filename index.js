1be1bb5f189e26f9f43309ba4cacf128c9aa4f17
Curtis Chu <curtischu@CurtisdeMacBook-Air.local>
2021-05-10 23:07:01 +0800

    完成第1題

---
+++ index.js
@@ -0,0 +1,82 @@
+
+let inputValue = {
+    hired: {
+        be: {
+            to: {
+                deserve: "I"
+            }
+        }
+    }
+}
+
+/*
+function v1(input) {
+    let [head] = reverse(input, 0);
+    return head;
+}
+
+function reverse(input, level) {
+    // hit the ground
+    if (typeof input !== 'object') {
+        let obj = {}
+        let head = obj;
+        return [head, input, obj];
+    }
+
+    let key = Object.keys(input)[0];
+    let value = input[key];
+
+    let [head, preKey, tail] = reverse(value, level + 1);
+
+    // compose the result at higher recursive level
+    if (level > 0) {
+        let newTail = {};
+        tail[preKey] = newTail;
+        return [head, key, newTail];
+    } else {
+        tail[preKey] = key;
+        return [head];
+    }
+
+}
+*/
+
+function v2(input) {
+    let root = true;
+    let output = {};
+
+    while (typeof input === 'object') {
+
+        if (Object.keys(input).length <= 0) {
+            throw 'no key was found.';
+        }
+
+        let key = Object.keys(input)[0];
+        let value = input[key];
+        
+        if (root) {
+            pre = key;
+            root = false;
+        } else {
+            output = { [key]: pre };
+            pre = output;
+        }
+
+        input = value;
+
+    }
+
+    // last one 
+    output = { [input]: pre };
+    return output;
+}
+
+const reverseObj = (input) => {
+    if(typeof input !== 'object' || Array.isArray(input))
+        throw 'Assume the input is an object.';
+
+    return v2(input);
+
+};
+console.log(JSON.stringify(reverseObj(inputValue), null, 2));
+
